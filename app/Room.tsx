6f12312e2a24d68c63aa8981a1f9d14ccd5201cd
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-03-19 18:23:40 -0600

    Add Room component

---
+++ app/Room.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { RoomProvider } from "@/liveblocks.config";
+import { ClientSideSuspense } from "@liveblocks/react";
+
+function Room({ children }: { children: React.ReactNode }) {
+  return (
+    <RoomProvider id='my-room' initialPresence={{}}>
+      <ClientSideSuspense fallback={<div>Loading...</div>}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+}
+
+export default Room;
